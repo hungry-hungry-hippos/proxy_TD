2ac1cbf64aedc1ac51114e5d7205923e6b669b04
td94 <quynhtrangdo94@gmail.com>
2019-02-21 15:03:15 -0800

    Change for docker deploy

--- proxy/server.js
+++ proxy/server.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const proxy = require('http-proxy-middleware');
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,3 +13,8 @@ app.listen(port, () => {
 });
 
 app.use('/:id', express.static(path.join(__dirname, 'public')));
+
+app.use('/restaurants/:id', proxy({target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com/'}));
+app.use('/articles/:id', proxy({target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com/'}));
+app.use('/reviews/:id', proxy({target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com/'}));
+app.use('/api/:id', proxy({target: 'http://ec2-54-67-103-109.us-west-1.compute.amazonaws.com/'}));
